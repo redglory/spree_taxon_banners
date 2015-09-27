fa338f5ecff79267d25b79a1dd560966ae2cbb50
redglory <pedropinto.inbox@gmail.com>
2015-09-27 18:14:24 +0100

    new custom icon style

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,4 +1,12 @@
 Spree::Taxon.class_eval do
+  # add custom size to icon and default to :normal size
+  has_attached_file :icon,
+      styles: { mini: '32x32>', normal: '128x128>', custom: '285x380>' },
+      default_style: :mini,
+      url: '/spree/taxons/:id/:style/:basename.:extension',
+      path: ':rails_root/public/spree/taxons/:id/:style/:basename.:extension',
+      default_url: '/assets/default_taxon.png'
+
   has_attached_file :banner,
       styles: { normal: '850x300>', preview: '440x140>' },
       default_style: :normal,
