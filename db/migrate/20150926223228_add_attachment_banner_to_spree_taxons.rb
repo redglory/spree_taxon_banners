d2e0255784b62f6eb736c70269ce4f76ba13e429
redglory <pedropinto.inbox@gmail.com>
2018-11-05 20:57:39 +0000

    convert to ActiveStorage

--- db/migrate/20150926223228_add_attachment_banner_to_spree_taxons.rb
+++ db/migrate/20150926223228_add_attachment_banner_to_spree_taxons.rb
@@ -1,4 +1,4 @@
-class AddAttachmentBannerToSpreeTaxons < ActiveRecord::Migration
+class AddAttachmentBannerToSpreeTaxons < ActiveRecord::Migration[5.0]
   def change
     add_attachment :spree_taxons, :banner
   end
